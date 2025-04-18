d13c44a2df6e709b5e79280411234725f690b5f3
hasnahamidan <hasnahamidan@gmail.com>
2025-04-18 23:40:52 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const clientId = "76776b067d794946aff978a99f961533";
-const redirectUri = "https://connectyourpulseid.vercel.app";
-const scope = "user-read-private user-read-email";
-
-document.getElementById("connectBtn").addEventListener("click", () => {
-  const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
-  window.location.href = authUrl;
-});
